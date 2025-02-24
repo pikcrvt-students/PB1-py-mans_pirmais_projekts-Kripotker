3aeb6055b3f36f61fa2c524cfc7b1555569bd7cd
Krists Krupniks <A240333KK@rvt.lv>
2025-02-24 09:51:46 +0200

    Darbs bez flush param

--- Enableit.bat
+++
@@ -1,1 +0,0 @@
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /t Reg_dword /v DisableRegistryTools /f /d 0
---
+++ Krupniks-Krists_izvade_flush.py
@@ -0,0 +1,11 @@
+#!/usr/env python
+"""
+Print function with flush param.
+Created by ...
+
+Url: https://www.includehelp.com/python/flush-parameter-in-python-with-print-function.aspx
+"""
+print("Hello world!" end(" ")
+sleep(5)
+print("Bye!!!")
+Input("press <ENTER> to continue...")
