f27e3caabc4172dcb1537f595589bf3f9954c5af
Krists Krupniks <A240333KK@rvt.lv>
2025-02-27 09:23:56 +0200

    Tiek izvadītas 8 no 8 animācijas grupām, kļūdas ir.

---
+++ Krists_Krupniks_Apgriezieni_ap_savu_asi.py
@@ -0,0 +1,50 @@
+#!/usr/Bin/env python
+"""
+Ascii Animācija. burtu rotacija ap centralo asi
+"""
+
+print("R  ")
+print(" O ")
+print("  T")
+print("   ")
+
+
+print(" R ")
+print(" O ")
+print(" T ")
+print("   ")
+
+
+print("  R")
+print(" O ")
+print("T  ")
+print("   ")
+
+
+print("   ")
+print("TOR")
+print("   ")
+print("   ")
+
+
+print("T  ")
+print(" O ")
+print("  R")
+print("   ")
+
+
+print(" T ")
+print(" O ")
+print(" R ")
+print("   ")
+
+
+print("  T")
+print(" O ")
+print("R  ")
+print("   ")
+
+
+print("   ")
+print("ROT")
+print("   ")
--- Krupniks-Krists_izvade_Datorspele.py
+++ Krupniks-Krists_izvade_Datorspele.py
@@ -9,31 +9,31 @@ def print_animation(text, delay=0.1, repeat=1):
             sys.stdout.write(char)
             sys.stdout.flush()
             time.sleep(delay)
-        print('')  # This creates a newline after each animation cycle.
-        time.sleep(0.5)  # Short pause between repetitions of the text.
+        print('')  
+        time.sleep(0.5)  
 
 print('Datorspēle "Uzmini skaitli".')
 print('Dators "Iedomajas" veselu skaitli no 1 līdz 10. ', end='\n\n')
 print('Uzmini skaitli. ')
 
-# Generate a random number between 1 and 10.
+
 Datora_skaitlis = randint(1, 10)
 
-# ASCII countdown animation
-countdown = ['3', '2', '1', 'Start!']
-for number in countdown:
-    print_animation(number, 1)
 
-# Game loop
+countdown = ['3', '2', '1']
+for number in countdown:
+    print_animation(number, 0.5)
+
+
 neuzmineja = True
 while neuzmineja:
     ievade = input('Ievadiet skaitli no 1 līdz 10, ieskaitot 10: ')
     
     try:
-        # Try to convert the input to an integer.
+
         Cilveka_skaitlis = int(ievade)
     except ValueError:
-        # If input is not an integer, prompt again.
+
         print('Lūdzu, ievadiet derīgu skaitli!')
         continue
     
@@ -42,7 +42,7 @@ while neuzmineja:
     elif Cilveka_skaitlis > Datora_skaitlis:
         print_animation('Neuzminējāt. Ievadiet mazāku skaitli. ', 0.1)
     else:
-        # If the guess is correct, break the loop.
+
         neuzmineja = False
 print('     ____.                                                  __  __')   
 print('    |    |__ __  ______  __ _____________  _______ _______ |__|/  |_ ')
