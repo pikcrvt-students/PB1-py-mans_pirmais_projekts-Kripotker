c9e034aac5d89fdda05cc756523356deba6b2c46
Krists Krupniks <A240333KK@rvt.lv>
2025-02-27 09:24:16 +0200

    Tiek izvadītas 8 no 8 animācijas grupām, kļūdas labotas.

---
+++ Krists_Krupniks_Apgriezieni_ap_savu_asi_v2.py
@@ -0,0 +1,64 @@
+#!/usr/Bin/env python
+
+
+"""
+Ascii Animācija. burtu rotacija ap centralo asi
+"""
+from time import sleep
+from os import system
+
+print("R  ")
+print(" O ")
+print("  T", flush=True)
+
+sleep(1)
+
+system('clear')
+print(" R ")
+print(" O ")
+print(" T ", flush=True)
+
+sleep(1)
+
+system('clear')
+print("  R")
+print(" O ")
+print("T  ", flush=True)
+
+sleep(1)
+
+system('clear')
+print("      ")
+print("T O R", flush=True)
+
+
+sleep(1)
+
+system('clear')
+print("T  ")
+print(" O ")
+print("  R", flush=True)
+
+sleep(1)
+
+system('clear')
+print(" T ")
+print(" O ")
+print(" R ", flush=True)
+
+sleep(1)
+system('clear')
+
+print("  T")
+print(" O ")
+print("R  ", flush=True)
+
+sleep(1)
+
+
+system('clear')
+print("R O T", flush=True)
+
+sleep(1)
+
+system('clear')
