2be4af3a08a447dbf81785714f57f5493e3d1644
Krists Krupņiks <a240333kk@rvt.lv>
2025-03-03 09:13:47 +0200

    ASCII Animācijas uzdevums nr.2 . Izvada 'cūskiņu ar šķērsli'

---
+++ Krists_Krupniks_animation.py
@@ -0,0 +1,99 @@
+#!/usr/Bin/env python
+
+
+"""
+Ascii Animācija. Čūskiņa apiet šķērsli
+"""
+from time import sleep
+from os import system
+
+print("")
+print("")
+print("")
+print(">")
+print("")
+print("")
+print("", flush=True)
+
+sleep(1)
+
+system('clear')
+print("")
+print("")
+print("")
+print("->")
+print("")
+print("")
+print("", flush=True)
+
+sleep(1)
+
+system('clear')
+print("")
+print("")
+print("")
+print("-->")
+print("")
+print("")
+print("", flush=True)
+
+sleep(1)
+
+system('clear')
+print("")
+print("")
+print("")
+print("--->")
+print("")
+print("")
+print("", flush=True)
+
+
+sleep(1)
+
+system('clear')
+print("")
+print("")
+print("")
+print("---->")
+print("")
+print("")
+print("", flush=True)
+
+sleep(1)
+
+system('clear')
+print("")
+print("")
+print("")
+print("----->")
+print("")
+print("")
+print("", flush=True)
+
+sleep(1)
+system('clear')
+
+print("")
+print("")
+print("")
+print("------>")
+print("")
+print("")
+print("", flush=True)
+
+sleep(1)
+
+
+system('clear')
+print("")
+print("")
+print("")
+print("------->")
+print("")
+print("")
+print("", flush=True)
+
+sleep(1)
+
+system('clear')
